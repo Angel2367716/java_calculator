1b39d9677abe9bfe92b54198a5f92d184dae4b7b
Marco Angel <angel2367716@gmail.com>
2020-07-29 20:39:53 -0700

    Added logic for multiplication and division
    Now the calculator can multiply, divice, add and substract. However the
    app still need to be refined since there isn't and equal sign button.
    Might need to research more the windowbuilder tool for eclipse.

--- src/java_calculator/Simple_calculator.java
+++ src/java_calculator/Simple_calculator.java
@@ -1,8 +1,11 @@
 package java_calculator;
 
 import java.awt.EventQueue;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -18,6 +21,20 @@ public class Simple_calculator {
 	private JTextField textFieldNumber2;
 	private JTextField textFieldAnswer;
 	private JLabel lblNewLabel_1;
+	private JButton btnNewButton_2;
+	private JLabel label;
+	private JLabel label_1;
+	private JLabel label_2;
+	private JLabel label_3;
+	private JLabel label_4;
+	private JLabel label_5;
+	private JLabel label_6;
+	private JLabel label_7;
+	private JLabel label_8;
+	private JLabel label_9;
+	private JLabel label_10;
+	private JLabel label_11;
+	private JButton btnNewButton_3;
 
 	/**
 	 * Launch the application.
@@ -51,33 +68,32 @@ public class Simple_calculator {
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setTitle("Marco's Java Calculator");
-		frame.getContentPane().setLayout(null);
+		frame.getContentPane().setLayout(new GridLayout(0, 4, 0, 0));
 
-		JLabel lblNewLabel = new JLabel("Simple Calculator App");
-		lblNewLabel.setBounds(10, 0, 416, 45);
+		label = new JLabel("");
+		frame.getContentPane().add(label);
+
+		JLabel lblNewLabel = new JLabel("More Info");
+		lblNewLabel.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				System.out.println("Test two");
+			}
+		});
+
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		frame.getContentPane().add(lblNewLabel);
 
+		label_1 = new JLabel("");
+		frame.getContentPane().add(label_1);
+
+		label_2 = new JLabel("");
+		frame.getContentPane().add(label_2);
+
 		textFieldNumber1 = new JTextField();
 		textFieldNumber1.setHorizontalAlignment(SwingConstants.CENTER);
-		textFieldNumber1.setBounds(71, 80, 75, 45);
 		frame.getContentPane().add(textFieldNumber1);
 		textFieldNumber1.setColumns(10);
-
-		textFieldNumber2 = new JTextField();
-		textFieldNumber2.setHorizontalAlignment(SwingConstants.CENTER);
-		textFieldNumber2.setBounds(332, 80, 75, 45);
-		frame.getContentPane().add(textFieldNumber2);
-		textFieldNumber2.setColumns(10);
-
-		textFieldAnswer = new JTextField();
-		textFieldAnswer.setBounds(156, 199, 130, 45);
-		frame.getContentPane().add(textFieldAnswer);
-		textFieldAnswer.setColumns(10);
-
-		lblNewLabel_1 = new JLabel("The Answer is");
-		lblNewLabel_1.setBounds(34, 214, 112, 14);
-		frame.getContentPane().add(lblNewLabel_1);
 
 		JButton btnNewButton = new JButton("+");
 		btnNewButton.addActionListener(new ActionListener() {
@@ -98,8 +114,34 @@ public class Simple_calculator {
 				}
 			}
 		});
-		btnNewButton.setBounds(183, 56, 56, 55);
 		frame.getContentPane().add(btnNewButton);
+
+		btnNewButton_2 = new JButton("*");
+		btnNewButton_2.addActionListener(new ActionListener() { 
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				int number1, number2, ans;
+				try {
+					number1 = Integer.parseInt(textFieldNumber1.getText());
+					number2 = Integer.parseInt(textFieldNumber2.getText());
+
+					ans = number1 * number2;
+					textFieldAnswer.setText(Integer.toString(ans));
+
+				} catch (Exception e) {
+					JOptionPane.showMessageDialog(null, "Please enter a valid number!");
+				}
+			}
+		});
+		frame.getContentPane().add(btnNewButton_2);
+
+		textFieldNumber2 = new JTextField();
+		textFieldNumber2.setHorizontalAlignment(SwingConstants.CENTER);
+		frame.getContentPane().add(textFieldNumber2);
+		textFieldNumber2.setColumns(10);
+
+		label_3 = new JLabel("");
+		frame.getContentPane().add(label_3);
 
 		JButton btnNewButton_1 = new JButton("-");
 		btnNewButton_1.addActionListener(new ActionListener() {
@@ -119,7 +161,58 @@ public class Simple_calculator {
 				}
 			}
 		});
-		btnNewButton_1.setBounds(183, 122, 56, 45);
 		frame.getContentPane().add(btnNewButton_1);
+
+		btnNewButton_3 = new JButton("/");
+		btnNewButton_3.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				int number1, number2, ans;
+
+				try {
+					number1 = Integer.parseInt(textFieldNumber1.getText());
+					number2 = Integer.parseInt(textFieldNumber2.getText());
+
+					ans = number1 / number2;
+					textFieldAnswer.setText(Integer.toString(ans));
+
+				} catch (Exception e) {
+					JOptionPane.showMessageDialog(null, "Please enter a valid number!");
+				}
+
+			}
+		});
+		frame.getContentPane().add(btnNewButton_3);
+
+		label_4 = new JLabel("");
+		frame.getContentPane().add(label_4);
+
+		label_5 = new JLabel("");
+		frame.getContentPane().add(label_5);
+
+		label_6 = new JLabel("");
+		frame.getContentPane().add(label_6);
+
+		label_7 = new JLabel("");
+		frame.getContentPane().add(label_7);
+
+		label_8 = new JLabel("");
+		frame.getContentPane().add(label_8);
+
+		label_9 = new JLabel("");
+		frame.getContentPane().add(label_9);
+
+		lblNewLabel_1 = new JLabel("The Answer is");
+		frame.getContentPane().add(lblNewLabel_1);
+
+		textFieldAnswer = new JTextField();
+		frame.getContentPane().add(textFieldAnswer);
+		textFieldAnswer.setColumns(10);
+
+		label_10 = new JLabel("");
+		frame.getContentPane().add(label_10);
+
+		label_11 = new JLabel("");
+		frame.getContentPane().add(label_11);
 	}
 }
